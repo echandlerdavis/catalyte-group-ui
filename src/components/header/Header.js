6adc4b249e1e2c6d407821d2968235e25ab1fc64
Devin Duval <dduval@catalyte.io>
2023-04-12 10:25:29 -0700

    shopping cart appearing in header. LINK NOT WORKING

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import GoogleLogin, { GoogleLogout } from 'react-google-login';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import loginUser from './HeaderService';
 import constants from '../../utils/constants';
 
@@ -35,7 +36,9 @@ const Header = () => {
    * @description Function to run if google login was unsuccessful
    */
   const handleGoogleLoginFailure = () => {
-    setGoogleError('There was a problem logging in with Google. Please wait and try again later.');
+    setGoogleError(
+      'There was a problem logging in with Google. Please wait and try again later.'
+    );
   };
 
   /**
@@ -52,13 +55,17 @@ const Header = () => {
    * @description Function to run if google logout was unsuccessful
    */
   const handleGoogleLogoutFailure = () => {
-    setGoogleError('There was a problem logging out with Google. Please wait and try again later.');
+    setGoogleError(
+      'There was a problem logging out with Google. Please wait and try again later.'
+    );
   };
 
   return (
     <div>
       <NavLink to="/home">Home</NavLink>
-      <NavLink to="/checkout">Cart</NavLink>
+      <ShoppingCartIcon>
+        <NavLink to="/checkout">Cart</NavLink>
+      </ShoppingCartIcon>
       {user && <span>{user.firstName}</span>}
       {user && <span>{user.lastName}</span>}
       {googleError && <span>{googleError}</span>}
