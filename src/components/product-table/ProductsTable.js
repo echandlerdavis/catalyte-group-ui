6f3bda5311625c7957da4014eee80661b7f4df9f
Kaschae Freeman <kaschaefreeman@gmail.com>
2023-04-19 14:34:34 -0400

    update products table to use material ui table component with pagination

--- src/components/product-table/ProductsTable.js
+++ src/components/product-table/ProductsTable.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TablePagination,
+  TableHead,
+  TableRow
+} from '@material-ui/core';
 import './ProductsTable.module.css';
 
 const ProductTable = ({ products }) => {
+  // Use state to set pagination options for the table
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = ({ target }) => {
+    setRowsPerPage(+target.value);
+    setPage(0);
+  };
+
   const productAttributes = [
     'id',
     'brand',
@@ -23,7 +45,11 @@ const ProductTable = ({ products }) => {
   ];
 
   // Map each product attribute to a table header
-  const tableHeaders = productAttributes.map((attribute) => <th key={attribute}>{attribute}</th>);
+  const tableHeaders = productAttributes.map((attribute) => {
+    const firstLetter = attribute.charAt(0).toUpperCase();
+    const restOfWord = attribute.slice(1);
+    return <TableCell key={attribute}>{firstLetter + restOfWord}</TableCell>;
+  });
 
   // Map the row data for each product
   const rowData = products.map(((product) => {
@@ -33,26 +59,40 @@ const ProductTable = ({ products }) => {
       const data = product[attribute];
       // If the value is a boolean get the string of the boolean
       return (
-        <td key={`${product.id} - ${attribute}`}>
+        <TableCell key={`${product.id} - ${attribute}`}>
           {typeof data === 'boolean' ? data.toString() : data}
-        </td>
+        </TableCell>
       );
     });
     // Return the row with each data column
-    return <tr key={product.id}>{productColumns}</tr>;
+    return <TableRow key={product.id}>{productColumns}</TableRow>;
   }));
 
   return (
     <section>
       <h2>Products</h2>
-      <table>
-        <thead>
-          <tr>{tableHeaders}</tr>
-        </thead>
-        <tbody>
-          {rowData}
-        </tbody>
-      </table>
+      <TableContainer>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {tableHeaders}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {/* Display row data for the number of rows chosen in pagination options */}
+            {rowData.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={rowData.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
     </section>
   );
 };
