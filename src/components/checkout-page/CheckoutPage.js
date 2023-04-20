c1d771264881ab87eebee84f1e7c4293deca115d
Kaschae Freeman <kaschaefreeman@gmail.com>
2023-04-20 19:59:45 -0400

    update checkout page to match app styling and added changed divs to semantic tags

--- src/components/checkout-page/CheckoutPage.js
+++ src/components/checkout-page/CheckoutPage.js
@@ -76,7 +76,7 @@ const CheckoutPage = () => {
     return (
       <div className={styles.checkoutContainer}>
         <div className={`${styles.step}${styles.order}`}>
-          <h3 className={styles.title}>1. Review Order</h3>
+          <h2 className={styles.title}>1. Review Order</h2>
           <div>Cart is empty. Please add your favorite items to get started!</div>
         </div>
       </div>
@@ -84,40 +84,46 @@ const CheckoutPage = () => {
   }
 
   return (
-    <div className={styles.checkoutContainer}>
-      <div className={`${styles.step} ${styles.order}`}>
-        <h3 className={styles.title}>1. Review Order</h3>
-        <ReviewOrderWidget />
-      </div>
-      <div className={`${styles.step} ${styles.delivery}`}>
-        <h3 className={styles.title}>2. Delivery Address</h3>
-        <DeliveryAddress onChange={onDeliveryChange} deliveryData={deliveryData} />
-        <label htmlFor="useSame" className={styles.sameAddressText}>
-          <div className={styles.useSameAddress}>
-            <input
-              id="useSame"
-              onChange={handleCheckboxChange}
-              type="checkbox"
-              checked={useSameAddress}
-            />
-          </div>
-          Same Billing Address
-        </label>
-      </div>
-      <div className={`${styles.step} ${styles.payment}`}>
-        <h3 className={styles.title}>3. Billing Details</h3>
-        <BillingDetails
-          onChange={onBillingChange}
-          billingData={billingData}
-          useShippingForBilling={useSameAddress}
-        />
-      </div>
+    <article className={styles.checkoutContainer}>
+      <section className={`${styles.step} ${styles.order}`}>
+        <h2 className={styles.title}>1. Review Order</h2>
+        <div className={`Card ${styles.stepCard}`}>
+          <ReviewOrderWidget />
+        </div>
+      </section>
+      <section className={`${styles.step} ${styles.delivery}`}>
+        <h2 className={styles.title}>2. Delivery Address</h2>
+        <div className={`Card ${styles.stepCard}`}>
+          <DeliveryAddress onChange={onDeliveryChange} deliveryData={deliveryData} />
+          <label htmlFor="useSame" className={styles.sameAddressText}>
+            <div className={styles.useSameAddress}>
+              <input
+                id="useSame"
+                onChange={handleCheckboxChange}
+                type="checkbox"
+                checked={useSameAddress}
+              />
+            </div>
+            Same Billing Address
+          </label>
+        </div>
+      </section>
+      <section className={`${styles.step} ${styles.payment}`}>
+        <h2 className={styles.title}>3. Billing Details</h2>
+        <div className={`Card ${styles.stepCard}`}>
+          <BillingDetails
+            onChange={onBillingChange}
+            billingData={billingData}
+            useShippingForBilling={useSameAddress}
+          />
+        </div>
+      </section>
       <div className={styles.payNow}>
         <button onClick={handlePay} type="button" className={styles.payButton}>
           Checkout
         </button>
       </div>
-    </div>
+    </article>
   );
 };
 
