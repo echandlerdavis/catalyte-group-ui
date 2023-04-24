9e0a5371af3eed36d9af82db041a167b37a5bc13
Casey Gandy <cgandy@catalyte.io>
2023-04-24 15:48:07 -0400

    edited syntax errors such as indentation size and line spaces

--- src/components/profile-page/profilepage.js
+++ src/components/profile-page/profilepage.js
@@ -1,42 +1,51 @@
-import React, {useState} from 'react';
-import styles from '.profilepage.module.css';
-import { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import styles from './profilepage.module.css';
 
 const ProfilePage = () => {
-    const [user, setUser] = useState(null);
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-    useEffect(() => {
-        fetch('/api/user')
-        .then ((response) => {
-            if (response.status === 401) {
-                setIsLoggedIn(false);
-                throw new Error('User not authenticated');
-            }
-            setIsLoggedIn(true);
-            return response.json();
-        })
-        .then((data) => setUser(data))
-        .catch((error) => console.error(error));
-    }, []);
+  useEffect(() => {
+    fetch('/api/user')
+      .then((response) => {
+        if (response.status === 401) {
+          setIsLoggedIn(false);
+          throw new Error('User not authenticated');
+        }
+        setIsLoggedIn(true);
+        return response.json();
+      })
+      .then((data) => setUser(data))
+      .catch((error) => console.error(error));
+  }, []);
 
-    if (!isLoggedIn) {
-        return <div>Please log in to continue to your user page</div>;
-    }
+  if (!isLoggedIn) {
+    return <div>Please log in to continue to your user page</div>;
+  }
 
-    if (!user) {
-        return <div>Loading...</div>;
-    }
+  if (!user) {
+    return <div>Loading...</div>;
+  }
 
-    return (
-        <div className={styles.container}>
-            <h1>{user.firstName} {user.lastName}'s Profile</h1>
-            <div className={styles.info}>
-                <p><strong>Email:</strong> {user.email}</p>
-                <p><strong>Role:</strong> {user.role}</p>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className={styles.container}>
+      <h1>
+        {user.firstName}
+        {user.lastName}
+        Profile
+      </h1>
+      <div className={styles.info}>
+        <p>
+          <strong>Email:</strong>
+          {user.email}
+        </p>
+        <p>
+          <strong>Role:</strong>
+          {user.role}
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default ProfilePage;
