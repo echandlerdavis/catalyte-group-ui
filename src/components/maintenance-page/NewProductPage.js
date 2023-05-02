36b3ad42464f96972a022f85f981c39039560ea2
Kaschae Freeman <kfreeman@catalyte.io>
2023-05-02 14:05:15 -0400

    update new product page with documentation and comments

--- src/components/maintenance-page/NewProductPage.js
+++ src/components/maintenance-page/NewProductPage.js
@@ -17,12 +17,23 @@ import {
 import AppAlert from '../alert/Alert';
 import constants from '../../utils/constants';
 
+/**
+ * @name NewProductPage
+ * @description component displays a form to create a new product
+ * @param history the browser history
+ * @param setApiError the setter function to set state of an API error for an api call
+ * @param setToastData the setter function that sets the data to be displayed in the toast component
+ * @param openToast the function that initializes the toast
+ * @returns JSX element, form with fields for product data
+ */
 const NewProductPage = ({
   history, setApiError, setToastData, openToast
 }) => {
   const date = new Date();
+  // Get today's date in format "YYYY-MM-DD"
   const initialReleaseDate = date.toISOString().split('T')[0];
 
+  // Declare initial form field values
   const initialFormData = {
     brand: '',
     imageSrc: '',
@@ -44,10 +55,10 @@ const NewProductPage = ({
 
   const [formData, setFormData] = useState(initialFormData);
   const [formErrorMessage, setFormErrorMessage] = useState(null);
+  const [distinctAttributes, setDistinctAtrributes] = useState({});
   const formHasError = useRef(false);
   const emptyFields = useRef([]);
   const priceIsInvalid = useRef(false);
-  const [distinctAttributes, setDistinctAtrributes] = useState({});
 
   const loadProductAttributeOptions = () => {
     GetProductBrands(setApiError, setDistinctAtrributes);
@@ -64,6 +75,7 @@ const NewProductPage = ({
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  // Declare input types for the product attribute fields
   const formInputTypes = {
     brand: 'text',
     imageSrc: 'text',
@@ -83,15 +95,29 @@ const NewProductPage = ({
     active: 'checkbox'
   };
 
+  // Validation functions for form fields
+
+  /**
+   * Generates a list of empty fields
+   * @returns array of field names that are empty
+   */
   const validateFieldsNotEmpty = () => (
     Object.keys(formData).filter((key) => formData[key].length === 0)
   );
 
+  /**
+   * Validates price has 2 digits after decimal
+   * @returns {boolean}
+   */
   const validatePriceTwoDecimals = () => {
     const priceArray = formData.price.split('.');
     return (priceArray.length !== 2 || priceArray[1].length !== 2);
   };
 
+  /**
+   * Calls empty field list generator and price validator
+   * reassigns the current reference value for the form error boolean
+   */
   const validateFormData = () => {
     emptyFields.current = validateFieldsNotEmpty();
     priceIsInvalid.current = validatePriceTwoDecimals();
@@ -102,14 +128,20 @@ const NewProductPage = ({
     }
   };
 
+  /**
+   * Generates the error message based on the form errors present
+   */
   const generateError = () => {
+    // Start with blank form error message to remove previous errors
     setFormErrorMessage(null);
     validateFormData();
+    // If fields are empty get list with empty fields and join the list in a string
     if (emptyFields.current.length) {
       setFormErrorMessage(constants.FORM_FIELDS_EMPTY(emptyFields.current));
     }
     if (priceIsInvalid.current) {
       const errorMessage = constants.PRODUCT_FORM_INVALID_PRICE;
+      // if the price is invalid check the form error message has the set empty fields string
       setFormErrorMessage((prev) => {
         if (prev) {
           return prev.concat(' AND ', errorMessage);
@@ -119,17 +151,28 @@ const NewProductPage = ({
     }
   };
 
+  /**
+   * Function that handles form input changes
+   * @param {EventTarget} target the target the triggered the event
+   */
   const handleFormChange = ({ target }) => {
     let { value } = target;
+    // If targe input was a checkbox set the value to be opposite of its current value
     if (target.type === 'checkbox') {
       value = !formData[target.id];
     }
+    // Update form data with the targets new value
     setFormData({
       ...formData,
       [target.id]: value
     });
   };
 
+  /**
+   * Function handles when form is submitted
+   * Generates the errors and saves the product if no errors present
+   * @param {Event} e
+   */
   const handleSubmit = (e) => {
     e.preventDefault();
     generateError();
@@ -146,14 +189,19 @@ const NewProductPage = ({
       {formHasError.current && <AppAlert severity="error" title="Error" message={formErrorMessage} />}
       <form className="Card" onSubmit={(e) => handleSubmit(e)}>
         <div className={styles.fieldContainer}>
-          {Object.keys(formInputTypes).map((attribute) => {
+          { // Map the form inputs to form items
+          Object.keys(formInputTypes).map((attribute) => {
             let styleClass = null;
+            // If the form attribute is listed as an empty field when errors are generated...
+            // Change the style of the input box
             if (emptyFields.current.length && emptyFields.current.includes(attribute)) {
               styleClass = styles.invalidField;
             }
+            // If the price is invalid change its input box style to an invalid field style
             if (attribute === 'price' && priceIsInvalid.current) {
               styleClass = styles.invalidField;
             }
+            // If the attribute has a distinct list of options map to an input with data list
             if (distinctAttributes && Object.keys(distinctAttributes).includes(attribute)) {
               return (
                 <FormItemDataList
@@ -168,6 +216,7 @@ const NewProductPage = ({
                 />
               );
             }
+            // Else return a regular form item
             return (
               <FormItem
                 key={attribute}
@@ -179,7 +228,8 @@ const NewProductPage = ({
                 className={styleClass}
               />
             );
-          })}
+          })
+          }
           <div className={styles.formButtonContainer}>
             <Button
               type="button"
