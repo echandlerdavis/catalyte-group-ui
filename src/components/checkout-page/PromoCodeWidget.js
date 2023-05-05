c6ecfbb5dde2c5945b09437c01b8093cbdc169b1
Devin Duval <dduval@catalyte.io>
2023-05-04 18:20:07 -0700

    got checkbox working and sketched out logic for validating promocode

--- src/components/checkout-page/PromoCodeWidget.js
+++ src/components/checkout-page/PromoCodeWidget.js
@@ -1,20 +1,46 @@
 import React, { useState } from 'react';
 import TextField from '@material-ui/core/TextField';
-import CheckBoxTwoTone from '@material-ui/icons/CheckBoxTwoTone';
+import CheckCircle from '@material-ui/icons/CheckCircle';
+import { green } from '@material-ui/core/colors';
 import Alert from '../alert/Alert';
+
+const checkBoxStyle = {
+  color: green[500],
+  fontSize: '4em'
+};
 
 const PromoCodeWidget = () => {
   const [errors, setErrors] = useState([]);
+  const [validCode, setValidCode] = useState(false);
+  const [code, setCode] = useState('');
+
+  const onUserInput = (e) => {
+    setCode(e.target.value);
+  };
 
   const onFocusChange = () => {
-    setErrors(['Error1, error2']);
+    // TODO: need helper function to hit DB to get code
+    if (code.length > 0) {
+      setErrors([]);
+      setValidCode(true);
+    } else {
+      setValidCode(false);
+      setErrors(['invalid code']);
+    }
   };
 
   return (
     <>
-      <TextField id="promocodeInput" label="PromoCode" variant="outlined" onFocusChange={() => onFocusChange()} />
+      <TextField
+        id="promocodeInput"
+        onChange={onUserInput}
+        label="PromoCode"
+        variant="outlined"
+        value={code}
+        onBlur={onFocusChange}
+      />
       { errors.length > 0 && <Alert severity="error" title="Error:" message={errors.join(',')} />}
-      {errors.length === 0 && <CheckBoxTwoTone />}
+      {errors.length === 0 && validCode && <CheckCircle style={checkBoxStyle} />}
     </>
   );
 };
