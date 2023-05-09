2f42fd2ef8c8b050b34bbedfaa6a5d1ce000a26b
Casey Gandy <cgandy@catalyte.io>
2023-05-09 12:45:21 -0400

    add Modal to remove product

--- src/components/checkout-page/ReviewOrderWidget.js
+++ src/components/checkout-page/ReviewOrderWidget.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useCart } from './CartContext';
 import OrderItem from './OrderItem';
 import { getSubtotal } from './ReviewOrderWidgetService';
@@ -10,22 +10,60 @@ import styles from './ReviewOrderWidget.module.css';
  * @return component
  */
 const ReviewOrderWidget = () => {
-  const {
-    state: { products }
-  } = useCart();
+  const { state: { products }, removeProducts } = useCart();
+  const [showModal, setShowModal] = useState(false);
+  const [productIdToRemove, setProductIdToRemove] = useState(null);
+
+  const handleRemoveConfirmation = (productId) => {
+    setProductIdToRemove(productId);
+    setShowModal(true);
+  };
+
+  const handleRemoveCancel = () => {
+    setProductIdToRemove(null);
+    setShowModal(false);
+  };
+
+  const handleRemoveProduct = () => {
+    if (productIdToRemove) {
+      removeProducts(productIdToRemove);
+    }
+    setShowModal(false);
+  };
+
   return (
     <>
+      {/* Order items */}
       {products.map(({
-        price, title, description, quantity
+        productId, price, title, description, quantity
       }) => (
         <OrderItem
-          key={title}
+          key={productId}
           price={price}
           title={title}
           description={description}
           quantity={quantity}
+          onRemoveConfirmation={handleRemoveConfirmation}
         />
       ))}
+
+      {/* Modal */}
+      {showModal && (
+        <div className={styles.modal}>
+          <div className={styles.modalContent}>
+            <p>Are you sure you want to remove this item from your cart?</p>
+            <div className={styles.modalButtons}>
+              <button className={styles.modalButton} onClick={handleRemoveCancel}>
+                Cancel
+              </button>
+              <button className={styles.modalButton} onClick={handleRemoveProduct}>
+                remove
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+      
       <hr />
       <div className={styles.subtotal}>
         <div>
