cc483208fb828cb61936eff916c97209488aa69e
Devin Duval <dduval@catalyte.io>
2023-05-10 08:31:10 -0700

    fixed spacing on error message

--- src/components/checkout-page/PromoCodeWidget.js
+++ src/components/checkout-page/PromoCodeWidget.js
@@ -2,23 +2,50 @@ import React, { useState, useEffect } from 'react';
 import TextField from '@material-ui/core/TextField';
 import CheckCircle from '@material-ui/icons/CheckCircle';
 import { green } from '@material-ui/core/colors';
-import Alert from '../alert/Alert';
+import { makeStyles } from '@material-ui/core/styles';
+import Alert from '@material-ui/lab/Alert';
 import fetchPromoCode from './PromoCodeWidgetService';
 
 const checkBoxStyle = {
   color: green[500],
   fontSize: '4em'
+
 };
 
 const promoCodeStyle = {
   display: 'flex',
-  fontSize: 10
+  fontSize: 10,
+  flexWrap: 'nowrap',
+  justifyContent: 'flex-start',
+  alignItems: 'center'
 };
+
+const createTextfieldClasses = makeStyles(() => ({
+  root: {
+    flexBasis: '50%',
+    margin: '1px'
+  }
+}));
+
+const createAlertClasses = makeStyles(() => ({
+  root: {
+    flexBasis: '50%',
+    border: 0,
+    margin: '1px',
+    paddingTop: '1px',
+    paddingBottom: '1px',
+    paddingLeft: '1px',
+    paddingRight: '4px'
+
+  }
+}));
 
 const PromoCodeWidget = ({ promoCode, setPromoCode }) => {
   const [errors, setErrors] = useState([]);
   const [code, setCode] = useState('');
   const [fetchData, setFetchData] = useState({});
+  const textClasses = createTextfieldClasses();
+  const alertClasses = createAlertClasses();
 
   const onUserInput = (e) => {
     setCode(e.target.value);
@@ -51,8 +78,9 @@ const PromoCodeWidget = ({ promoCode, setPromoCode }) => {
         variant="outlined"
         value={code}
         onBlur={onFocusChange}
+        classes={textClasses}
       />
-      { errors.length > 0 && <Alert severity="error" title="Error:" message={errors.join(': ')} />}
+      { errors.length > 0 && <Alert severity="error" classes={alertClasses}>{errors.join(': ')}</Alert>}
       {errors.length === 0 && promoCode && <CheckCircle style={checkBoxStyle} />}
     </div>
   );
