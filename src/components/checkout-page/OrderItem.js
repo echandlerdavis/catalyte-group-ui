4bdfd0016afbd81165e6942ea589014b47be55b2
Casey Gandy <cgandy@catalyte.io>
2023-05-11 10:36:11 -0400

    add trashcan icon to cart for option to remove items

--- src/components/checkout-page/OrderItem.js
+++ src/components/checkout-page/OrderItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import DeleteOutlineIcon from '@material-ui/icons/DeleteOutlined';
 import styles from './OrderItem.module.css';
 import { toPrice } from './ReviewOrderWidgetService';
 
@@ -8,24 +9,38 @@ import { toPrice } from './ReviewOrderWidgetService';
  * @return component
  */
 const OrderItem = ({
-  price, title, description, quantity, onRemove
-}) => (
-  <div className={styles.orderItem}>
-    <div className={styles.image}>
-      IMAGE HERE
-    </div>
-    <div className={styles.item}>
-      <p className={styles.itemTitle}>{title}</p>
-      <p>{description}</p>
-      <p>{`Qty: ${quantity}`}</p>
-    </div>
-    <div className={styles.price}>
-      <p>{toPrice(quantity * price)}</p>
-      <button type="button" onClick={onRemove} className={styles.removeButton}>
-        <i className="fa fa-trash" />
-      </button>
-    </div>
-  </div>
-);
+  price, title, description, quantity, onRemoveConfirmation
+}) => {
+  const handleRemoveConfirmation = () => {
+    onRemoveConfirmation();
+  };
 
+  return (
+    <div className={styles.orderItem}>
+      <div className={styles.image}>
+        IMAGE HERE
+      </div>
+      <div className={styles.item}>
+        <p className={styles.itemTitle}>{title}</p>
+        <p>{description}</p>
+        <p>{`Qty: ${quantity}`}</p>
+      </div>
+      <div className={styles.price}>
+        <p>{toPrice(quantity * price)}</p>
+        <button
+          type="button"
+          onClick={handleRemoveConfirmation}
+          style={{
+            backgroundColor: 'transparent',
+            border: 'none',
+            padding: 0,
+            cursor: 'pointer'
+          }}
+        >
+          <DeleteOutlineIcon />
+        </button>
+      </div>
+    </div>
+  );
+};
 export default OrderItem;
